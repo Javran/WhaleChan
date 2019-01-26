3a0521faa4b65ba6aea1cf33da2696fd772c5a87
Javran Cheng <Javran.c@gmail.com>
2019-01-25 23:54:15 -0800

    separate ReoccuringEvents

--- src/Javran/WhaleChan/Main.hs
+++ src/Javran/WhaleChan/Main.hs
@@ -24,6 +24,8 @@ import Data.Time.LocalTime
 import Data.Time.LocalTime.TimeZone.Olson
 import Data.Time.LocalTime.TimeZone.Series
 
+import Javran.WhaleChan.ReoccuringEvents
+
 {-
   architecture draft:
 
@@ -119,98 +121,6 @@ timerThread = forever $ do
     let timeRep = formatTime defaultTimeLocale (iso8601DateFormat $ Just "%H:%M:%S%Q") t'
     putStrLn $ "Woke up at " ++ timeRep
 -- TODO: use lens-datetime
-localDayAdd :: Int -> LocalTime -> LocalTime
-localDayAdd n lt@LocalTime{localDay = ld} = lt {localDay = addDays (fromIntegral n) ld}
-
--- TODO: setup tests
-
-nextPracticeReset :: LocalTime -> LocalTime
-nextPracticeReset lt@LocalTime{localTimeOfDay = TimeOfDay {todHour}}
-  | todHour < 3 = todayAt3
-  | todHour < 15 = todayAt15
-  | otherwise = localDayAdd 1 todayAt3
-  where
-    todayAt3 = lt {localTimeOfDay = TimeOfDay 3 0 0}
-    todayAt15 = lt {localTimeOfDay = TimeOfDay 15 0 0}
-
-nextDailyQuestReset :: LocalTime -> LocalTime
-nextDailyQuestReset lt@LocalTime{localTimeOfDay = TimeOfDay {todHour}}
-  | todHour < 5 = todayAt5
-  | otherwise = localDayAdd 1 todayAt5
-  where
-    todayAt5 = lt {localTimeOfDay = TimeOfDay 5 0 0}
-
-nextWeeklyQuestReset :: LocalTime -> LocalTime
-nextWeeklyQuestReset lt@LocalTime{localDay, localTimeOfDay = TimeOfDay {todHour}}
-  | dayOfWeek /= 1 = nextMondayAt5
-  | todHour < 5 = todayAt5
-  | otherwise = nextMondayAt5
-  where
-    todayAt5 = lt {localTimeOfDay = TimeOfDay 5 0 0}
-    nextMondayAt5 = localDayAdd (8 - dayOfWeek) todayAt5
-    (_, _, dayOfWeek) = toWeekDate localDay
-
-nextMonthlyQuestReset :: LocalTime -> LocalTime
-nextMonthlyQuestReset LocalTime{localDay, localTimeOfDay = TimeOfDay {todHour}}
-  | day /= 1 = nextFirstDayAt5
-  | todHour < 5 = firstDayAt5
-  | otherwise = nextFirstDayAt5
-  where
-    (year, month, day) = toGregorian localDay
-    at5 = TimeOfDay 5 0 0
-    firstDay = fromGregorian year month 1
-    firstDayAt5 = LocalTime firstDay at5
-    nextFirstDayAt5 = LocalTime (addGregorianMonthsClip 1 firstDay) at5
-
-nextQuarterlyQuestReset :: LocalTime -> LocalTime
-nextQuarterlyQuestReset lt@LocalTime{localDay, localTimeOfDay = TimeOfDay {todHour}}
-  | month /= nextResetMonth || day /= 1 = nextQuarterFirstDayAt5
-  | todHour < 5 = lt {localTimeOfDay = at5 }
-  | otherwise = nextQuarterFirstDayAt5
-  where
-    (year, month, day) = toGregorian localDay
-    nextResetMonth = 3 * ceiling @Double @Int (fromIntegral month / 3)
-    at5 = TimeOfDay 5 0 0
-    nextQuarterFirstDayAt5 =
-      LocalTime (fromGregorian year nextResetMonth 1) at5
-
-nextExtraOperationReset :: LocalTime -> LocalTime
-nextExtraOperationReset LocalTime{localDay} = LocalTime lDay (TimeOfDay 0 0 0)
-  where
-    (year, month, _) = toGregorian localDay
-    lDay = addGregorianMonthsClip 1 (fromGregorian year month 1)
-
-{-
-  usually 2:00 JST and 14:00 JST, but for the last day of a month, there's an additional 22:00 JST
- -}
-nextSenkaAccounting :: LocalTime -> LocalTime
-nextSenkaAccounting lt@LocalTime{localTimeOfDay = TimeOfDay {todHour}}
-  | todHour < 2 = todayAt2
-  | todHour < 14 = lt { localTimeOfDay = TimeOfDay 14 0 0 }
-  | day == lastDay && todHour < 22 = lt { localTimeOfDay = TimeOfDay 22 0 0 }
-  | otherwise =
-    -- next day at 22
-    LocalTime (addDays 1 (localDay todayAt2)) (TimeOfDay 2 0 0)
-  where
-    todayAt2 = lt { localTimeOfDay = TimeOfDay 2 0 0 }
-    (year, month, day) = toGregorian (localDay lt)
-    lastDay = gregorianMonthLength year month
-
-nextQuestPointDeadline :: LocalTime -> LocalTime
-nextQuestPointDeadline lt@LocalTime{localTimeOfDay = TimeOfDay {todHour}}
-  | day /= lastDay = thisMonthDdl
-  | todHour < 14 = thisMonthDdl
-  | otherwise =
-      -- compute as if we are in first day of next month
-      -- given that this will always satisfy day /= lastDay, we are safe from infinite loops
-      nextQuestPointDeadline $
-        LocalTime
-          (addGregorianMonthsClip 1 (fromGregorian year month 1))
-          (TimeOfDay 0 0 0)
-  where
-    thisMonthDdl = LocalTime (fromGregorian year month lastDay) (TimeOfDay 14 0 0)
-    (year, month, day) = toGregorian (localDay lt)
-    lastDay = gregorianMonthLength year month
 
 startService :: WEnv -> IO ()
 startService _ = do
