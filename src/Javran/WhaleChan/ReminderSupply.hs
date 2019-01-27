aa488e74c3c4b57e02c895aedbdcdbc9f9c624ba
Javran Cheng <Javran.c@gmail.com>
2019-01-27 00:05:06 -0800

    framework: reminder supply

---
+++ src/Javran/WhaleChan/ReminderSupply.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TypeApplications #-}
+module Javran.WhaleChan.ReminderSupply
+  ( DailyQuestReset
+  ) where
+
+import Data.Time.Clock
+import Data.Time.LocalTime
+import Data.Time.LocalTime.TimeZone.Series
+
+import Javran.WhaleChan.Types
+import Javran.WhaleChan.ReoccuringEvents
+
+renewSupplyByFunc :: (LocalTime -> LocalTime) -> TimeZoneSeries -> UTCTime -> [UTCTime]
+renewSupplyByFunc getNextTime tzs ut =
+    [mkTime 30, mkTime 10, mkTime 5]
+  where
+    toLocal = utcToLocalTime' tzs
+    fromLocal = localTimeToUTC' tzs
+    eventTime :: UTCTime
+    eventTime = fromLocal . getNextTime . toLocal $ ut
+    mkTime mins = addUTCTime (fromIntegral @Int $ mins * 60) eventTime
+
+data DailyQuestReset
+
+instance ReminderSupply DailyQuestReset where
+    renewSupply _ = renewSupplyByFunc nextDailyQuestReset
--- src/Javran/WhaleChan/Types.hs
+++ src/Javran/WhaleChan/Types.hs
@@ -1,10 +1,18 @@
-{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+{-# LANGUAGE
+    RecordWildCards
+  , OverloadedStrings
+  , ExplicitForAll
+  , ExistentialQuantification
+  #-}
 module Javran.WhaleChan.Types where
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
 import qualified Web.Telegram.API.Bot as Tg
+import Data.Time.Clock
+import Data.Time.LocalTime.TimeZone.Series
 import Data.Aeson
+import Data.Typeable
 
 data WEnv = WEnv
   { twConsumerKey :: BS.ByteString
@@ -15,20 +23,30 @@ data WEnv = WEnv
   } deriving (Show)
 
 instance ToJSON WEnv where
-  toJSON WEnv {..}
-    | Tg.Token tok <- tgBotToken
-    = object [ "twitter-consumer-key" .= BSC.unpack twConsumerKey
-             , "twitter-consumer-secret" .= BSC.unpack twConsumerSecret
-             , "twitter-oauth-token" .= twOAuthToken
-             , "twitter-oauth-secret" .= twOAuthSecret
-             , "telegram-bot-token" .= tok
-             ]
+    toJSON WEnv {..}
+      | Tg.Token tok <- tgBotToken
+      = object [ "twitter-consumer-key" .= BSC.unpack twConsumerKey
+               , "twitter-consumer-secret" .= BSC.unpack twConsumerSecret
+               , "twitter-oauth-token" .= twOAuthToken
+               , "twitter-oauth-secret" .= twOAuthSecret
+               , "telegram-bot-token" .= tok
+               ]
 
 instance FromJSON WEnv where
-  parseJSON = withObject "WEnv" $ \o ->
-      WEnv
-        <$> (BSC.pack <$> o .: "twitter-consumer-key")
-        <*> (BSC.pack <$> o .: "twitter-consumer-secret")
-        <*> o .: "twitter-oauth-token"
-        <*> o .: "twitter-oauth-secret"
-        <*> (Tg.Token <$> o .: "telegram-bot-token")
+    parseJSON = withObject "WEnv" $ \o ->
+        WEnv
+            <$> (BSC.pack <$> o .: "twitter-consumer-key")
+            <*> (BSC.pack <$> o .: "twitter-consumer-secret")
+            <*> o .: "twitter-oauth-token"
+            <*> o .: "twitter-oauth-secret"
+            <*> (Tg.Token <$> o .: "telegram-bot-token")
+
+{-
+  a ReminderSupply, when given current time,
+  supplies a sorted list of times for the timer thread
+ -}
+class ReminderSupply r where
+    renewSupply :: forall p. p r -> TimeZoneSeries -> UTCTime -> [UTCTime]
+
+data EReminderSupply =
+  forall rs. (ReminderSupply rs, Typeable rs) => ERS (Proxy rs)
