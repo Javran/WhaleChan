ce6e852244161fc1aadfe466450b7a9d1068b0f6
Javran Cheng <Javran.c@gmail.com>
2019-02-17 23:24:51 -0800

    maintenance time parser

--- src/Javran/WhaleChan/NextMaintenance.hs
+++ src/Javran/WhaleChan/NextMaintenance.hs
@@ -6,14 +6,25 @@ module Javran.WhaleChan.NextMaintenance where
 import Network.HTTP.Client
 
 import qualified Data.ByteString.Lazy as BSL
-import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8)
+import Text.ParserCombinators.ReadP
 
 import Javran.WhaleChan.Types
 
 {-
   for figuring out next maintenance time
  -}
+
+mTimeFieldP :: String -> ReadP String
+mTimeFieldP fd =
+    string fd *> skipSpaces *>
+    string "=" *> skipSpaces *> string "Date.parse(\"" *>
+    munch1 (/='\"') <* string "\");" <* skipSpaces <* eof
+
+mStartTimeP, mEndTimeP :: ReadP String
+mStartTimeP = mTimeFieldP "MaintenanceInfo.StartDateTime"
+mEndTimeP = mTimeFieldP "MaintenanceInfo.EndDateTime"
 
 {-
   possible sources:
@@ -24,12 +35,12 @@ import Javran.WhaleChan.Types
   - Wikia: https://kancolle.fandom.com/wiki/Recent_Updates
  -}
 
-decodeFromRaw :: BSL.ByteString -> Text
-decodeFromRaw = decodeUtf8 . BSL.toStrict
+decodeFromRaw :: BSL.ByteString -> String
+decodeFromRaw = T.unpack . decodeUtf8 . BSL.toStrict
 
 getInfoFromGameSource :: WEnv -> IO ()
 getInfoFromGameSource (_,TCommon{tcManager}) = do
     req <- parseRequest "http://203.104.209.7/gadget_html5/js/kcs_const.js"
     resp <- httpLbs req tcManager
-    print (decodeFromRaw $ responseBody resp)
+    putStrLn (decodeFromRaw $ responseBody resp)
     pure ()
