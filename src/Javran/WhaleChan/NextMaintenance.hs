a10a5e92783732525ad1ebe7e57f6a0f97dd2222
Javran Cheng <Javran.c@gmail.com>
2019-02-17 23:49:28 -0800

    server info parsing done

--- src/Javran/WhaleChan/NextMaintenance.hs
+++ src/Javran/WhaleChan/NextMaintenance.hs
@@ -5,12 +5,21 @@ module Javran.WhaleChan.NextMaintenance where
 
 import Network.HTTP.Client
 
+import Control.Applicative
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 import Data.Text.Encoding (decodeUtf8)
 import Text.ParserCombinators.ReadP
+import qualified Data.IntMap as IM
+import Data.Maybe
+import Data.Either
 
 import Javran.WhaleChan.Types
+
+data KCSConst = KCSConst
+  { serverInfo :: IM.IntMap String
+  , maintenanceTime :: (String, String)
+  } deriving (Show)
 
 {-
   for figuring out next maintenance time
@@ -37,6 +46,22 @@ serverInfoP =
               <* string "\";" <* skipSpaces <* eof
             )
 
+kcsConstFromRaw :: String -> Maybe KCSConst
+kcsConstFromRaw = tr . mapMaybe parseLine . lines
+  where
+    -- all-in-one parser that run through all lines
+    allP = (Left <$> serverInfoP)
+        <|> (Right . Left <$> mStartTimeP)
+        <|> (Right . Right <$> mEndTimeP)
+    parseLine inp = case readP_to_S allP inp of
+        [(a,"")] -> Just a
+        _ -> Nothing
+    tr :: [Either (Int, String) (Either String String)] -> Maybe KCSConst
+    tr xs = case partitionEithers mTimes of
+        ([sT], [eT]) -> Just $ KCSConst (IM.fromList sInfos) (sT,eT)
+        _ -> Nothing
+      where
+        (sInfos, mTimes) = partitionEithers xs
 {-
   possible sources:
 
@@ -53,5 +78,5 @@ getInfoFromGameSource :: WEnv -> IO ()
 getInfoFromGameSource (_,TCommon{tcManager}) = do
     req <- parseRequest "http://203.104.209.7/gadget_html5/js/kcs_const.js"
     resp <- httpLbs req tcManager
-    putStrLn (decodeFromRaw $ responseBody resp)
+    print (kcsConstFromRaw $ decodeFromRaw $ responseBody resp)
     pure ()
