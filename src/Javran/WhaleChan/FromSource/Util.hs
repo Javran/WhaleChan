681eeaeb60d1700769c11871f6c334560843c6bb
Javran Cheng <Javran.c@gmail.com>
2019-02-18 21:39:46 -0800

    fetchUrl

--- src/Javran/WhaleChan/FromSource/Util.hs
+++ src/Javran/WhaleChan/FromSource/Util.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE
     OverloadedStrings
   , FlexibleContexts
+  , NamedFieldPuns
   #-}
 module Javran.WhaleChan.FromSource.Util
   ( tell'
   , expectOne
+  , fetchUrl
   ) where
 
 import Control.Monad
 import Control.Monad.Writer
+import Network.HTTP.Client
+
+import qualified Data.ByteString.Lazy as BSL
 
 tell' :: MonadWriter (Endo [a]) m => a -> m ()
 tell' x = tell $ Endo ([x] ++)
@@ -19,3 +24,8 @@ expectOne tag (x:xs) = do
     unless (null xs) $
       tell' $ "extra result for " ++ tag ++ ": " ++ show xs
     pure (Just x)
+
+fetchUrl :: Manager -> String -> IO BSL.ByteString
+fetchUrl mgr url = do
+    req <- parseRequest url
+    responseBody <$> httpLbs req mgr
