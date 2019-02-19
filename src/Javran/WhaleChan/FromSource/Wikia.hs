dc90f3960e1a128b73a6d86520c8aebbed456dfb
Javran Cheng <Javran.c@gmail.com>
2019-02-18 20:32:50 -0800

    refactor: expectOne

--- src/Javran/WhaleChan/FromSource/Wikia.hs
+++ src/Javran/WhaleChan/FromSource/Wikia.hs
@@ -2,7 +2,6 @@
     OverloadedStrings
   , TupleSections
   , FlexibleContexts
-  , NoMonomorphismRestriction
   #-}
 module Javran.WhaleChan.FromSource.Wikia where
 
@@ -16,6 +15,8 @@ import Text.XML.Cursor
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Text as T
 
+import Javran.WhaleChan.Util
+
 parseMaintenanceTime :: BSL.ByteString -> ((Maybe T.Text, Maybe T.Text), [String])
 parseMaintenanceTime =
     second (`appEndo` [])
@@ -25,22 +26,11 @@ parseMaintenanceTime =
     . parseLBS
 
 collectResult :: [(Bool, T.Text)] -> ((Maybe T.Text, Maybe T.Text), Endo [String])
-collectResult xs = runWriter $ do
-  let (starts, ends) = partition fst xs
-      tell' x = tell $ Endo ([x] ++)
-  mS <- case starts of
-    [] -> tell' "missing start time" >> pure Nothing
-    (_,y):ys -> do
-      unless (null ys) $
-        tell' $ "extra start times: " ++ show (snd <$> ys)
-      pure (Just y)
-  mE <- case ends of
-    [] -> tell' "missing end time" >> pure Nothing
-    (_,y):ys -> do
-      unless (null ys) $
-        tell' $ "extra end times: " ++ show (snd <$> ys)
-      pure (Just y)
-  pure (mS, mE)
+collectResult xs
+  | (starts, ends) <- partition fst xs
+  = runWriter $
+      (,) <$> expectOne "start time" (snd <$> starts)
+          <*> expectOne "end time" (snd <$> ends)
 
 searchAndExtractCountdownStrs :: Cursor -> [(Bool, T.Text)]
 searchAndExtractCountdownStrs =
