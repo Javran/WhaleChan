6344eb4f76583de89ba4a24634b775390cc93ba5
Javran Cheng <Javran.c@gmail.com>
2019-02-21 23:41:57 -0800

    adding IO version with loggerIO provided

--- src/Javran/WhaleChan/Log.hs
+++ src/Javran/WhaleChan/Log.hs
@@ -3,6 +3,7 @@
   #-}
 module Javran.WhaleChan.Log
   ( d, i, w, e
+  , d', i', w', e'
   ) where
 
 {-
@@ -11,8 +12,16 @@ module Javran.WhaleChan.Log
 
 import Control.Monad.Logger
 
+type LoggerIO = Loc -> LogSource -> LogLevel -> LogStr -> IO ()
+
 withLvl :: (MonadLogger m, ToLogStr a, ToLogStr b) => LogLevel -> a -> b -> m ()
 withLvl lvl who msg = logWithoutLoc "" lvl (toLogStr who <> ": " <> toLogStr msg)
+
+withLvlIO :: (ToLogStr a, ToLogStr b)
+          => LogLevel
+          -> LoggerIO -> a -> b -> IO ()
+withLvlIO lvl loggerIO who msg =
+    runLoggingT (withLvl lvl who msg) loggerIO
 
 d,i,w,e :: MonadLogger m => String -> String -> m ()
 
@@ -20,3 +29,10 @@ d = withLvl LevelDebug
 i = withLvl LevelInfo
 w = withLvl LevelWarn
 e = withLvl LevelError
+
+d',i',w',e' :: LoggerIO -> String -> String -> IO ()
+
+d' = withLvlIO LevelDebug
+i' = withLvlIO LevelInfo
+w' = withLvlIO LevelWarn
+e' = withLvlIO LevelError
