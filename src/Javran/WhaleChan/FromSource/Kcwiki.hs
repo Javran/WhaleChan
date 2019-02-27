81d85eb972d9e50e3c8d5bb00feb5e536b39de3f
Javran Cheng <Javran.c@gmail.com>
2019-02-27 00:24:14 -0800

    Kcwiki parsing

--- src/Javran/WhaleChan/FromSource/Kcwiki.hs
+++ src/Javran/WhaleChan/FromSource/Kcwiki.hs
@@ -2,9 +2,11 @@
     OverloadedStrings
   , FlexibleContexts
   , NoMonomorphismRestriction
+  , TupleSections
   #-}
 module Javran.WhaleChan.FromSource.Kcwiki where
 
+import Control.Applicative
 import Control.Arrow
 import Control.Monad.Writer
 import Text.HTML.DOM
@@ -15,15 +17,36 @@ import qualified Data.Text as T
 
 import Javran.WhaleChan.FromSource.Util
 import Javran.WhaleChan.FromSource.TimeFormat
+import Data.List
 
-parseTime :: BSL.ByteString -> (Maybe T.Text, [String])
+-- TODO: share code with Wikia
+
+parseTime :: BSL.ByteString -> ((Maybe T.Text, Maybe T.Text), [String])
 parseTime =
     second (`appEndo` [])
-    . runWriter
-    . expectOne "start time"
-    . ($// searchAndExtract)
+    . collectResult
+    . ($// searchAndExtractCountdownStrs)
     . fromDocument
     . parseLBS
+
+collectResult :: [(Bool, T.Text)] -> ((Maybe T.Text, Maybe T.Text), Endo [String])
+collectResult xs
+  | (ends{- True (in front) for end-}, starts) <- partition fst xs
+  = runWriter $
+      (,) <$> expectOne "start time" (snd <$> starts)
+          <*> expectOne "end time" (snd <$> ends)
+
+searchAndExtractCountdownStrs :: Cursor -> [(Bool, T.Text)]
+searchAndExtractCountdownStrs =
+    element "span"
+    >=> "class" `attributeIs` "countdown"
+    >=> checkAndTag
+    >=> (\(b,t) -> (b,) <$> (t $| attribute "data-until"))
+  where
+    -- extract only relevant tags with tag attached to distinguish between start and end
+    checkAndTag =
+        (<|>) <$> (("data-type" `attributeIs` "maint-start") &| (False,))
+              <*> (("data-type" `attributeIs` "maint-end") &| (True,))
 
 searchAndExtract :: Cursor -> [T.Text]
 searchAndExtract =
@@ -36,10 +59,6 @@ parse t = eitherToMaybe $ mkTimeParser "%Y/%-m/%-d %T %z" (T.unpack t)
 
 getInfo :: Manager -> IO (PRange UTCTime)
 getInfo mgr = do
-    raw <- fetchUrl mgr "https://zh.kcwiki.org/wiki/Template:维护倒数?action=render"
-    let (lRaw, _) = parseTime raw
-    {-
-      TODO as one rhs is always Nothing, we are effectively ignoring this source,
-      this can only be restored once Kcwiki support both maintenance start and end time.
-     -}
-    pure (toPRange (lRaw >>= parse) Nothing)
+    raw <- fetchUrl mgr "https://zh.kcwiki.org/wiki/Template:维护倒数?action=raw"
+    let ((lRaw, rRaw), _) = parseTime raw
+    pure (toPRange (lRaw >>= parse) (rRaw >>= parse))
