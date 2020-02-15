3a5acfebbda7ebc2048365dbdcc5d7ea43c1920b
Javran Cheng <Javran.C@gmail.com>
2020-02-14 23:28:31 -0800

    compute twinfo from dhall config

--- src/Javran/WhaleChan/Dhall.hs
+++ src/Javran/WhaleChan/Dhall.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE
-    DeriveGeneric
+    OverloadedStrings
+  , RecordWildCards
+  , NamedFieldPuns
+  , DeriveGeneric
   #-}
 module Javran.WhaleChan.Dhall where
 
 import Dhall
+import Web.Twitter.Conduit
+import Data.Text.Encoding (encodeUtf8)
 
 -- This is the config loaded by Dhall,
 -- which we will eventually turn into WConf.
@@ -18,3 +23,18 @@ data PreConf = PreConf
   , tgChannelId :: Integer
   } deriving (Show, Generic)
 
+getTwInfo :: PreConf -> TWInfo
+getTwInfo PreConf{..} = def { twToken, twProxy }
+  where
+    twOAuth =
+        twitterOAuth
+        { oauthConsumerKey = encodeUtf8 twConsumerKey
+        , oauthConsumerSecret = encodeUtf8 twConsumerSecret
+        }
+    twCredential =
+        Credential
+        [ ("oauth_token", encodeUtf8 twOAuthToken)
+        , ("oauth_token_secret", encodeUtf8 twOAuthSecret)
+        ]
+    twToken = def { twOAuth, twCredential }
+    twProxy = Nothing
